8fda1a00633e9c96363ca59b41e84e18ffb6910d
bhiggbozz <plutonish007@yahoo.com>
2026-02-28 05:59:15 +0100

    scripts

--- TechHub.Entity.Migration/Migration/Scripts.sql
+++ TechHub.Entity.Migration/Migration/Scripts.sql
@@ -41,3 +41,62 @@ CREATE TABLE Subjects
     IsActive BIT NOT NULL
         CONSTRAINT DF_Subjects_IsActive DEFAULT (1)
 );
+
+--------------------------------------------------------------------------------------
+
+ CREATE TABLE ClassroomSubject
+(
+    Id UNIQUEIDENTIFIER NOT NULL 
+        CONSTRAINT PK_ClassroomSubjects PRIMARY KEY,
+
+    CreationDate DATETIME2(7) NOT NULL 
+        CONSTRAINT DF_ClassroomSubjects_CreationDate 
+        DEFAULT SYSDATETIME(),
+
+    ModifiedDate DATETIME2(7) NOT NULL 
+        CONSTRAINT DF_ClassroomSubjects_ModifiedDate 
+        DEFAULT SYSDATETIME(),
+
+    ClassroomId UNIQUEIDENTIFIER NOT NULL,
+    SubjectId UNIQUEIDENTIFIER NOT NULL,
+    SchoolId UNIQUEIDENTIFIER NOT NULL,
+    CreatedBy UNIQUEIDENTIFIER NOT NULL,
+
+    IsActive BIT NOT NULL 
+        CONSTRAINT DF_ClassroomSubjects_IsActive DEFAULT 1,
+
+    -- Foreign Keys
+    CONSTRAINT FK_ClassroomSubjects_Classroom 
+        FOREIGN KEY (ClassroomId) 
+        REFERENCES Classroom(Id),
+
+    CONSTRAINT FK_ClassroomSubjects_Subject 
+        FOREIGN KEY (SubjectId) 
+        REFERENCES Subjects(Id),
+
+    CONSTRAINT FK_ClassroomSubjects_School 
+        FOREIGN KEY (SchoolId) 
+        REFERENCES School(Id)
+);
+
+------------------------------------------------------
+ CREATE TABLE ClassroomTeacher (
+    Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    ClassroomId UNIQUEIDENTIFIER NOT NULL,
+    TeacherId UNIQUEIDENTIFIER NOT NULL,
+    IsPrimary BIT DEFAULT 0,  -- Designate primary/head teacher for classroom
+    CreationDate DATETIME NOT NULL DEFAULT GETUTCDATE(),
+    ModifiedDate DATETIME NOT NULL DEFAULT GETUTCDATE(),
+    CreatedBy UNIQUEIDENTIFIER NOT NULL,
+    SchoolId UNIQUEIDENTIFIER NOT NULL,
+    IsActive BIT DEFAULT 1,
+    
+    -- Foreign keys
+    CONSTRAINT FK_ClassroomTeacher_Classroom FOREIGN KEY (ClassroomId) REFERENCES Classroom(Id),
+    CONSTRAINT FK_ClassroomTeacher_Teacher FOREIGN KEY (TeacherId) REFERENCES Users(Id),
+    CONSTRAINT FK_ClassroomTeacher_School FOREIGN KEY (SchoolId) REFERENCES School(Id),
+    CONSTRAINT FK_ClassroomTeacher_CreatedBy FOREIGN KEY (CreatedBy) REFERENCES Users(Id),
+    
+    -- Unique constraint (one teacher can't be assigned to same classroom twice)
+    CONSTRAINT UQ_ClassroomTeacher_Classroom_Teacher UNIQUE (ClassroomId, TeacherId)
+);
