cd1cbba4dd3b1ea8e2d1314a3d91687f6d936e5a
Gbiggs001 <sunday-om@Ha-shem.com>
2026-05-17 21:58:34 +0200

    rabbitmq

--- TechHub.Core/Configuration/RabbitMQSettings.cs
+++ TechHub.Core/Configuration/RabbitMQSettings.cs
@@ -1,12 +1,13 @@
-namespace TechHub.Core.Configuration;
+﻿namespace TechHub.Core.Configuration;
 
 public class RabbitMQSettings
 {
-    public const string SectionName = "RabbitMQ";
-
-    public string Host { get; set; } = "localhost";
-    public int Port { get; set; } = 5672;
-    public string Username { get; set; } = "guest";
-    public string Password { get; set; } = "guest";
-    public string BoardBatchQueue { get; set; } = "board.batch.queue";
+	public const string SectionName = "RabbitMQ";
+	public string AmqpUrl        { get; set; }  // ← add
+    public string Host           { get; set; }
+    public int    Port           { get; set; }
+    public string Username       { get; set; }
+    public string Password       { get; set; }
+    public string VirtualHost    { get; set; }
+    public string BoardBatchQueue { get; set; }
 }
