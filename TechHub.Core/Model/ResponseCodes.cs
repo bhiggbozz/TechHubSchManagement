1490de28b4530cf11cbad5282f04e018ae5a100a
Sunday Omoyele <Sunday.Omoyele@Interswitchng.com>
2026-06-26 22:12:14 +0100

    responseCOde

--- TechHub.Core/Model/ResponseCodes.cs
+++ TechHub.Core/Model/ResponseCodes.cs
@@ -11,7 +11,7 @@ namespace TechHub.Core.Model
 		public const string BadRequest = "99001";
 		public const string successful = "99000";
 		public const string ErrorOccured = "99101";
-		public const string Forbidden = "99103";
+		public const string Forbidden = "AX1003";
 		public const string Unauthorized = "99107";
 		public const string Conflict = "99161";
 		public const string NotFound = "99134";
